8f6016481275eba3caaf99de07eac9ab6529c32b
QaysTrad <qaystrad@gmail.com>
2018-03-24 09:49:06 +0200

    finish the Big O

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,9 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
-
+1 - Big-O(1) : it's instant no changes on the memory  , like a return , best case 
+2 - Big-O(logn) : this when not that big changes on the memory  , like the if statment 
+3 - Big-O(n)  :it's constant make some changes on the memory  , like the one for loop , you need to go over every elemnt 
+4 - Big-O(n2) : this when has a hevely changes on the memory and take some times , like a two nisted for loop  , it take much time to loop in every elemnt inside it
+5 - Big-O(con) : this is the worst in them it may freze your device and take the most time in them , so try to avoid it , this the bad case 
 */
